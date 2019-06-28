a726c42f47d4aedb9a90bb2b1e71d46cc1178074
Unknown <ruggero_93@live.it>
2019-06-28 15:06:12 +0200

    Change name

---
+++ ChooseYourWine/src/app/Search/Search.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Search',
+  templateUrl: 'Searc.page.html',
+  styleUrls: ['Search.page.scss']
+})
+export class SearchPage {
+
+  constructor() {}
+
+}
