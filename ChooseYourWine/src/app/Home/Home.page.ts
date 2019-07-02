b5c80b090f6b6b259c1278a5f9a16539b934ed0d
Unknown <ruggero_93@live.it>
2019-07-02 16:49:24 +0200

    http non funzionante

--- ChooseYourWine/src/app/Home/Home.page.ts
+++ ChooseYourWine/src/app/Home/Home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptionsArgs, Headers, URLSearchParams } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 
 @Component({
@@ -6,8 +10,34 @@ import { Component } from '@angular/core';
   templateUrl: 'Home.page.html',
   styleUrls: ['Home.page.scss']
 })
+@Injectable()
 export class HomePage {
+  constructor(private http: Http) { }
 
-  constructor() {}
+  private sparkqlData = null;
 
+  sparkql() {
+    let headers: Headers = new Headers({
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    });
+
+    let params = new URLSearchParams();
+    params.append('query', 'SELECT * WHERE { <http://dbpedia.org/resource/Awolnation> ?pref ?obj } LIMIT 10');
+    params.append('format', 'json');
+
+    let options: RequestOptionsArgs = {
+      headers: headers,
+      params: URLSearchParams
+    };
+
+    this.http.get('http://dbpedia.org/sparql', options) // 1
+      .pipe(map(response => response.json()))
+      .subscribe(data => {
+          console.log(data);
+          this.sparkqlData = data; // 3
+       });
+     console.log(this.sparkqlData);  // 2
+  }
 }
+
