ef38746dbfc7c4ef4989629a7f00e00b37b1401b
Mariangela Petraglia <40724691+MaryPet91@users.noreply.github.com>
2019-07-02 22:28:28 +0200

    Changed Home.page.ts

--- ChooseYourWine/src/app/Home/Home.page.ts
+++ ChooseYourWine/src/app/Home/Home.page.ts
@@ -2,8 +2,6 @@ import { Component} from '@angular/core';
 import { Http, RequestOptionsArgs, Headers, URLSearchParams } from '@angular/http';
 import { map } from 'rxjs/operators';
 
-
-
 @Component({
   selector: 'app-Home',
   templateUrl: 'Home.page.html',
@@ -20,28 +18,22 @@ export class HomePage {
       'Accept':'application/json',
       'Access-Control-Allow-Origin':'*',
       'Access-Control-Allow-Credentials':'true',
-      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS,POST,PUT',
       'Access-Control-Allow-Headers':'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     });
 
-    console.log("qua ci sono");
     let params = new URLSearchParams();
     let query = 'PREFIX vino: <http://w3id.org/food/ontology/disciplinare-vino#> SELECT ?Nome ?Tipologia WHERE { ?x vino:haDenominazione ?Nome. ?Nome vino:haTipologia ?Tipologia. }';
     params.append('query', query);
     params.append('format', 'json');
-
+  
     let options: RequestOptionsArgs = {
       headers: headers,
       params: params
     };
-    console.log("pure qua");
-    this.http.get('http://localhost:8890/sparql', options) // 1
-      //.pipe(map(response => response.json()))
-      .subscribe(data => {
+    this.http.post('http://localhost:8890/sparql', options).subscribe(data => {
           console.log(data);
-          this.sparkqlData = data; // 3
+          this.sparkqlData = data;
        });
-     console.log(this.sparkqlData);  // 2
   }
 }
 
