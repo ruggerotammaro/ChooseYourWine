9b4e94ce9f58e547b303a485b1f5c4cefbd04de4
Mariangela Petraglia <40724691+MaryPet91@users.noreply.github.com>
2019-07-02 19:03:51 +0200

    Update Home.pages.ts

--- ChooseYourWine/src/app/Home/Home.page.ts
+++ ChooseYourWine/src/app/Home/Home.page.ts
@@ -12,26 +12,30 @@ import { map } from 'rxjs/operators';
 
 export class HomePage {
   constructor(private http: Http) { }
-
   private sparkqlData = null;
 
   sparkql() {
     let headers: Headers = new Headers({
-      'Content-type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json'
+      'Content-type':'application/x-www-form-urlencoded',
+      'Accept':'application/json',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Credentials':'true',
+      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Headers':'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     });
 
     console.log("qua ci sono");
     let params = new URLSearchParams();
-    params.append('query', 'SELECT * WHERE { <http://dbpedia.org/resource/Awolnation> ?pref ?obj } LIMIT 10');
+    let query = 'PREFIX vino: <http://w3id.org/food/ontology/disciplinare-vino#> SELECT ?Nome ?Tipologia WHERE { ?x vino:haDenominazione ?Nome. ?Nome vino:haTipologia ?Tipologia. }';
+    params.append('query', query);
     params.append('format', 'json');
 
     let options: RequestOptionsArgs = {
       headers: headers,
-      params: URLSearchParams
+      params: params
     };
     console.log("pure qua");
-    this.http.get('http://dbpedia.org/sparql', options) // 1
+    this.http.get('http://localhost:8890/sparql', options) // 1
       //.pipe(map(response => response.json()))
       .subscribe(data => {
           console.log(data);
