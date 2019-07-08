93bba18c92ff394f43da9ab13ecc3aba581ff7bc
ruggero tammaro <ruggero_93@live.it>
2019-07-08 12:00:08 +0200

    logic Search

--- ChooseYourWine/src/app/Search/Search.page.ts
+++ ChooseYourWine/src/app/Search/Search.page.ts
@@ -12,16 +12,15 @@ export class SearchPage {
 
   vini:any;
   pas:any;
+  tipo:any;
   constructor(private http: Http, private router: Router,private route:ActivatedRoute) {
-    this.vini=[];
-    let query = 'SELECT+%3FNomiVini+%0D%0AWHERE+%7B+%3Fx+vino%3AhaDenominazione+%3FNomiVini.';
-    this.query(query);    
+    this.vini=[];  
    }
   private sparkqlData = null;
   public jsonObject: any;
 
   query(query){
-    this.http.get('http://localhost:8890/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2FwineIMG&query=PREFIX+vino%3A+%3Chttp%3A%2F%2Fw3id.org%2Ffood%2Fontology%2Fdisciplinare-vino%23%3E+%0D%0A'+query+'+%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on').pipe(map(
+    this.http.get('http://localhost:8890/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2FwineIMG&query=PREFIX+vino%3A+%3Chttp%3A%2F%2Fw3id.org%2Ffood%2Fontology%2Fdisciplinare-vino%23%3E+%0D%0A'+query+'+format=application%2Fsparql-results%2Bjson&timeout=0&debug=on').pipe(map(
     res => res.json())).subscribe( data => {this.jsonObject = data;
       this.sparkqlData=JSON.stringify(this.jsonObject);
       this.getAllWine(this.sparkqlData);      
@@ -52,7 +51,7 @@ export class SearchPage {
     let obj = JSON.parse(allWine);
     let n = obj.results.bindings.length;
     for (var i = 0; i < n; i++) {
-      this.vini.push(this.splittingString(obj.results.bindings[i].NomiVini.value));
+      this.vini.push(this.splittingString(obj.results.bindings[i].denominazione.value));
     }
   }
 
@@ -67,5 +66,13 @@ export class SearchPage {
   convert(vino){
     var x= vino.replace(/ /g,"_");
     return x;
+  }
+
+
+  search(tipologia){
+    this.vini=[];
+    this.tipo=tipologia.target.value;
+    let query = 'select+%3Fdenominazione%0D%0Awhere%7B%0D%0A%3Fx+vino%3AhaDenominazione+%3Fdenominazione.%0D%0A%3Fdenominazione+vino%3AhaTipologia+%3Ftipologia%0D%0AFILTER+regex%28%3Ftipologia%2C%22'+this.tipo+'%22%29%7D&';
+    this.query(query);    
   }
 }
